38e366b8b77ca737d544ff1e3518883e0cdccff8
Imtiyaaz-D <imtdamon777@gmail.com>
2023-08-18 14:39:06 +0200

    update

---
+++ challenger_gui/src/components/FooterComp.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <footer id="footer">
+            <div id="copyright">
+                <p>@POWERED BY Imti-Damon COPYRIGHT &COPY; {{cYear}}</p>
+                <div class="icons">
+                    <a href="https://github.com/Imtiyaaz-D" target="_blank"><i class="bi bi-github"></i></a>
+                <a href="https://www.linkedin.com/feed/" target="_blank"><i class="bi bi-linkedin"></i></a>
+                <a href="https://www.instagram.com/imtiyaazdamon/" target="_blank"><i class="bi bi-instagram"></i></a>
+                </div>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return{
+                cYear: new Date().getUTCFullYear()
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+.bi:hover {
+    color: blue;
+}
+
+.bi {
+
+    font-size: 1.8rem;
+    margin: 0.5rem;
+    color: black;
+}
+</style>
--- challenger_gui/src/router/index.js
+++ challenger_gui/src/router/index.js
@@ -14,6 +14,26 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path:'/product',
+    name: 'product',
+    component: () => import('../views/ProductsView.vue')
+  },
+  {
+    path:'/contact',
+    name: 'contact',
+    component: () => import('../views/ContactView.vue')
+  },
+  {
+    path:'/login',
+    name:'login',
+    component:()=> import('../views/LoginView.vue')
+  },
+  {
+    path:'/users',
+    name:'users',
+    component:()=> import('../views/UsersView..vue')
   }
 ]
 
